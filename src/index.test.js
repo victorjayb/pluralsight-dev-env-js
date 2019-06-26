1b170406c0c296f41493a07ce0c16860ea6433bb
victorjayb <victorjayb@yahoo.com>
2019-06-26 11:10:59 +0100

    configure DOM testing with jsdom

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,19 @@
 import { expect } from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+const { JSDOM } = jsdom;
 
 describe('Our first test', () => {
   it('should pass', () => {
     expect(true).to.equal(true);
+  });
+});
+
+describe('index.html', () => {
+  it('should say hello', () => {
+    const index = fs.readFileSync('./src/index.html', 'utf-8');
+    const dom = new JSDOM(index);
+    expect(dom.window.document.querySelector('h1').innerHTML).to.equal('Hello World!');
   });
 });
